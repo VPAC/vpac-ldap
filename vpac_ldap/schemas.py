fd05e2b7f053d003cc1136c3378589976ac5c04f
Brian May <brian@microcomaustralia.com.au>
2012-12-10 13:49:29 +1100

    New terminology.

--- vpac_ldap/schemas.py
+++ vpac_ldap/schemas.py
@@ -78,7 +78,7 @@ class rfc_group(rfc.posixGroup, samba.sambaGroupMapping, common.baseMixin):
 
     # accounts
     primary_accounts = tldap.manager.OneToManyDescriptor(this_key='gidNumber', linked_cls=rfc_account, linked_key='gidNumber', related_name="primary_group")
-    secondary_accounts = tldap.manager.ManyToManyDescriptor(this_key='memberUid', linked_cls=rfc_account, linked_key='uid', linked_has_foreign_key=False, related_name="secondary_groups")
+    secondary_accounts = tldap.manager.ManyToManyDescriptor(this_key='memberUid', linked_cls=rfc_account, linked_key='uid', linked_is_p=False, related_name="secondary_groups")
 
 ######
 # ad #
